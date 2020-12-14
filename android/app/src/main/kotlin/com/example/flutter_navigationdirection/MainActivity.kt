268c4f1575ad89bd2245e6ec01d3866312fc3e4c
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-14 15:51:10 +0530

    FLUT-3178 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/flutter_navigationdirection/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_navigationdirection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(NavigationDirection());
+
+class NavigationDirection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Card(
+            margin: const EdgeInsets.fromLTRB(50, 100, 50, 100),
+            child: SfDateRangePicker(
+              navigationDirection: DateRangePickerNavigationDirection.vertical,
+            ),
+          ),
+        ),
+
+        // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
